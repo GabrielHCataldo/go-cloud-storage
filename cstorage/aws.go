e1cd634dc848f90f86397e663eb8e2b81069863a
Gabriel Cataldo <gabriel.cataldo@petzpartner.com.br>
2024-01-08 05:22:56 -0300

    Finish google storage documentation

--- cstorage/aws.go
+++ cstorage/aws.go
@@ -1,1 +1,10 @@
 package cstorage
+
+import "cloud.google.com/go/storage"
+
+type awsS3Storage struct {
+	client *storage.Client
+}
+
+type AwsS3Storage interface {
+}
--- cstorage/google.go
+++ cstorage/google.go
@@ -10,49 +10,78 @@ import (
 	"google.golang.org/api/option"
 )
 
+// CreateGoogleBucketInput input for creating a Google storage bucket
 type CreateGoogleBucketInput struct {
+	// BucketName name of the bucket to be created (required)
 	BucketName string `validate:"required"`
-	ProjectId  string `validate:"required"`
-	Attrs      *storage.BucketAttrs
+	// ProjectId project id where the bucket will be created (required)
+	ProjectId string `validate:"required"`
+	// Attrs metadata options to customize bucket creation
+	Attrs *storage.BucketAttrs
 }
 
+// UploadFileInput input for creating/updating a new object in the bucket
 type UploadFileInput struct {
-	BucketName string       `validate:"required"`
-	FileName   string       `validate:"required"`
-	MimeType   FileMimeType `validate:"required,enum"`
-	Content    any          `validate:"required"`
+	// BucketName name of the bucket where the object will be created (required)
+	BucketName string `validate:"required"`
+	// FileName name of the object that will be created (required)
+	FileName string `validate:"required"`
+	// MimeType type of content of the object that will be created (required)
+	MimeType FileMimeType `validate:"required,enum"`
+	// Content of the object that will be created (required)
+	Content any `validate:"required"`
 }
 
+// UploadFilesOutput output for creating/updating multiple objects in the bucket
 type UploadFilesOutput struct {
+	// Attrs metadata for the object created/updated
 	Attrs *storage.ObjectAttrs
-	Err   error
+	// Err error occurred when creating/updating the object
+	Err error
 }
 
+// DeleteFileInput input for removing an object from the bucket
 type DeleteFileInput struct {
+	// BucketName name of the bucket where the object will be deleted (required)
 	BucketName string `validate:"required"`
-	FileName   string `validate:"required"`
+	// FileName name of the object to be deleted (required)
+	FileName string `validate:"required"`
 }
 
+// DeleteFolderInput input to remove a folder (prefix) of objects from the bucket
 type DeleteFolderInput struct {
+	// BucketName name of the bucket where the objects will be deleted (required)
 	BucketName string `validate:"required"`
+	// FolderName folder name (prefix) where the objects will be deleted (required)
 	FolderName string `validate:"required"`
 }
 
+// DeleteFileOutput output of removing several objects from the bucket
 type DeleteFileOutput struct {
+	// BucketName name of the bucket where the object was deleted
 	BucketName string
-	FileName   string
-	Err        error
+	// FileName name of the object that was deleted
+	FileName string
+	// Err error occurred when deleting the object
+	Err error
 }
 
+// DeleteFolderOutput output of removing multiple object folders from bucket
 type DeleteFolderOutput struct {
+	// BucketName name of the bucket where the objects were deleted
 	BucketName string
+	// FolderName name of the folder (prefix) that was deleted
 	FolderName string
-	Err        error
+	// Err an error occurred while deleting objects from the folder
+	Err error
 }
 
+// DeleteBucketOutput output of removing multiple buckets
 type DeleteBucketOutput struct {
+	// BucketName deleted bucket name
 	BucketName string
-	Err        error
+	// Err an error occurred while deleting the bucket
+	Err error
 }
 
 type googleStorage struct {
@@ -60,30 +89,43 @@ type googleStorage struct {
 }
 
 type GoogleStorage interface {
+	// CreateBucket creates the Bucket in the project
+	// If attrs is nil the API defaults will be used
 	CreateBucket(ctx context.Context, input CreateGoogleBucketInput) error
+	// GetBucket returns the metadata for the bucket
 	GetBucket(ctx context.Context, bucketName string) (*storage.BucketAttrs, error)
+	// UploadFile set the value passed in the indicated bucket
 	UploadFile(ctx context.Context, input UploadFileInput) (*storage.ObjectAttrs, error)
+	// UploadFiles set multiple values passed in the indicated bucket
 	UploadFiles(ctx context.Context, inputs ...UploadFileInput) []UploadFilesOutput
+	// FindOneByName returns the metadata for the object by name
 	FindOneByName(ctx context.Context, bucketName, fileName string) (*storage.ObjectAttrs, error)
+	// Find returns a list of object metadata, customize the search using the opts parameter (OptionFind)
 	Find(ctx context.Context, bucketName string, opts ...OptionFind) ([]storage.ObjectAttrs, error)
+	// DeleteFile deletes the single specified object
 	DeleteFile(ctx context.Context, input DeleteFileInput) error
+	// DeleteFiles deletes multiple objects specified in the input
 	DeleteFiles(ctx context.Context, inputs ...DeleteFileInput) []DeleteFileOutput
+	// DeleteFolder deletes all objects from a folder (prefix)
 	DeleteFolder(ctx context.Context, input DeleteFolderInput) error
+	// DeleteFolders deletes all objects from all folders (prefix) mentioned in the input
 	DeleteFolders(ctx context.Context, input ...DeleteFolderInput) []DeleteFolderOutput
+	// DeleteBucket deletes the Bucket
 	DeleteBucket(ctx context.Context, bucketName string) error
+	// DeleteBuckets deletes multiple buckets mentioned in the input
 	DeleteBuckets(ctx context.Context, bucketName ...string) []DeleteBucketOutput
+	// Disconnect close connect to google storage
 	Disconnect() error
+	// SimpleDisconnect close connect to google storage, without error
 	SimpleDisconnect()
 }
 
+// NewGoogleStorage new instance of connection with google storage, to close it just use Disconnect() or SimpleDisconnect()
 func NewGoogleStorage(ctx context.Context, opts ...option.ClientOption) (GoogleStorage, error) {
 	client, err := storage.NewClient(ctx, opts...)
-	if helper.IsNotNil(err != nil) {
-		return nil, err
-	}
 	return googleStorage{
 		client: client,
-	}, nil
+	}, err
 }
 
 func (g googleStorage) CreateBucket(ctx context.Context, input CreateGoogleBucketInput) error {
